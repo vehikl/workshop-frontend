37481388a02b2b0789565f67c388ad6af5a8abd1
Vehikl Team <go@vehikl.com>
2022-04-08 14:08:38 -0400

    remove clutter

--- src/components/TodoList.spec.ts
+++ src/components/TodoList.spec.ts
@@ -1,20 +1,14 @@
 import {mount} from "@vue/test-utils";
 import TodoList from "./TodoList.vue";
 
-function foobar() {
-    return 2;
-}
-
 describe('todo-list', () => {
     it('adds a new todo-item to the list when I enter it in the input box and click on submit', async () => {
         const wrapper = mount(TodoList)
 
         await wrapper.find('input[name=new-item]').setValue("Do the dishes");
 
-        // Press the save button
         await wrapper.find('button').trigger('click');
 
-        // Expect to see "Do the dishes" appearing within the list of items
         expect(wrapper.find('ol').text()).toContain("Do the dishes");
     });
 
@@ -32,7 +26,7 @@ describe('todo-list', () => {
         expect(wrapper.find('ol').text()).toContain("Another thing");
     })
 
-    it("disabled the add/submit button if the input is empty", async () => {
+    it("disables the add/submit button if the input is empty", async () => {
         const wrapper = mount(TodoList)
 
         await wrapper.find('input[name=new-item]').setValue("");
